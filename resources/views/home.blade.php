55fa10c13c799dbc4dea29ad40fa0ffad88211ac
Relison Galvão <relison@cd10.com.br>
2021-10-06 18:25:39 -0300

    Home CSS

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,31 +3,134 @@
 @section('description', 'Uma plataforma de transparência pública colaborativa, que reúne dados das organizações da sociedade civil de todo o Brasil')
 @section('content')
 
-
-
-
-
-
-
-
-    <div class="space">&nbsp;</div>
+    <div class="p-3">&nbsp;</div>
     <div class="container">
         <div class="row">
-            <div class="col-md-12">
-                <div id="nextOsc"></div>
+            <div class="col">
+                <div class="dorder-container">
+                    <div class="dorder-container-mai">
+                        <div class="btn-icon">
+                            <img src="img/icon-diagnostico.png" alt="Diagnóstico" title="Diagnóstico" width="100%">
+                        </div>
+                         <h2 class="btn-icon-h2">Diagnóstico</h2>
+                        <div class="clear-both"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="dorder-container">
+                    <div class="dorder-container-mai">
+                        <div class="btn-icon">
+                            <img src="img/icon-biblioteca.png" alt="Diagnóstico" title="Diagnóstico" width="100%">
+                        </div>
+                        <h2 class="btn-icon-h2">Biblioteca</h2>
+                        <div class="clear-both"></div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 
-    <div style="background-color: #F9F9F9;">
-        <br><br><br>
+    <div class="container-fluid">
+        <div class="p-3">&nbsp;</div>
+        <div class="dorder-container">
+            <div class="bg-lgt dorder-container-mai">
+                <div class="dorder-container-line">
+                    <h2>Como funciona?</h2>
+                    <div class="dorder-container-box bg-lgt"></div>
+                </div>
+            </div>
+        </div>
+        <div class="p-3">&nbsp;</div>
+    </div>
 
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 text-center">
+                <img src="img/p1.png" alt="" title="" width="70%">
+            </div>
+            <div class="col-md-3 text-center">
+                <h2>1</h2>
+            </div>
+            <div class="col-md-6">
+                <h3>Diagnóstico</h3>
+                <p>O diagnóstico visa identificar e avaliar riscos de desatenção, tratamento inadequado e exclusão de segmentos específicos do público atendido. Muitas vezes, esses riscos não são suficientemente bem conhecidos.</p>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-2">&nbsp;</div>
+            <div class="col-md-3  text-center">
+                <img src="img/arrow1.png" alt="" title="" width="40%">
+                <br><br>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-3 text-center">
+                <h2>2</h2>
+            </div>
+            <div class="col-md-6">
+                <h3>Diagnóstico</h3>
+                <p>O diagnóstico visa identificar e avaliar riscos de desatenção, tratamento inadequado e exclusão de segmentos específicos do público atendido. Muitas vezes, esses riscos não são suficientemente bem conhecidos.</p>
+            </div>
+            <div class="col-md-3 text-center">
+                <img src="img/p2.png" alt="" title="" width="70%">
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-2">&nbsp;</div>
+            <div class="col-md-3  text-center">
+                <img src="img/arrow2.png" alt="" title="" width="40%">
+                <br><br>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-3 text-center">
+                <img src="img/p3.png" alt="" title="" width="70%">
+            </div>
+            <div class="col-md-3 text-center">
+                <h2>3</h2>
+            </div>
+            <div class="col-md-6">
+                <h3>Diagnóstico</h3>
+                <p>O diagnóstico visa identificar e avaliar riscos de desatenção, tratamento inadequado e exclusão de segmentos específicos do público atendido. Muitas vezes, esses riscos não são suficientemente bem conhecidos.</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="p-3">&nbsp;</div>
+    <div class="bg-lgt bg-lines">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <img src="img/p4.png" alt="" title="" width="70%">
+                </div>
+                <div class="col-md-8">
+                    <h2>“Amei! Me ajudou muito! Me fez enxergar questões que não via antes!” </h2>
+                    <h3>Maria lima</h3>
+                </div>
+            </div>
+        </div>
     </div>
 
 
 
-    <div class="space">&nbsp;</div>
 
+    <div class="container-fluid">
+        <div class="p-3">&nbsp;</div>
+        <div class="dorder-container">
+            <div class="bg-lgt dorder-container-mai">
+                <div class="dorder-container-line">
+                    <h2>Parceiros</h2>
+                    <div class="dorder-container-box bg-lgt"></div>
+                </div>
+            </div>
+        </div>
+        <div class="p-3">&nbsp;</div>
+    </div>
 
 <script>
 
