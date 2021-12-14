7af423a066bd01cc9bb9150047ac8dec9b0c5ef3
Bruno Passos <brpassos.programador@gmail.com>
2021-12-14 16:30:29 -0300

    INC-79

--- public/js/components/diagnostico/index.js
+++ public/js/components/diagnostico/index.js
@@ -4,7 +4,12 @@ const Diagnostico = () => {
   } = React;
   const context = useContext(DiagnosticoContext);
   return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement(Header, null), context.tipo ? /*#__PURE__*/React.createElement(Dimensoes, null) : /*#__PURE__*/React.createElement("br", null) //null
-  );
+  , context.tipo ? /*#__PURE__*/React.createElement("div", {
+    className: "text-center"
+  }, /*#__PURE__*/React.createElement("button", {
+    className: "btn btn-lg btn-primary",
+    onClick: () => context.enviarRespostas()
+  }, "Enviar Respostas")) : null, /*#__PURE__*/React.createElement("br", null));
 };
 
 ReactDOM.render( /*#__PURE__*/React.createElement(DiagnosticoProvider, null, /*#__PURE__*/React.createElement(Diagnostico, null)), document.getElementById('diagnostico'));
--- resources/js/components/diagnostico/index.js
+++ resources/js/components/diagnostico/index.js
@@ -9,6 +9,14 @@ const Diagnostico = () => {
             {
                 context.tipo ? <Dimensoes/> : <br/> //null
             }
+            {
+                context.tipo ? (
+                    <div className="text-center">
+                        <button className="btn btn-lg btn-primary" onClick={() => context.enviarRespostas()}>Enviar Respostas</button>
+                    </div>
+                ) : null
+            }
+            <br/>
         </div>
     );
 };
