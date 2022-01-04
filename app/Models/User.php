273593af70843be61b174ffcde1b8f304381e7a1
raphael <raphael.s.abreu@gmail.com>
2022-01-04 10:52:32 -0300

    Colocando users no schema public para fazer funcionar o login

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,7 @@ class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
-    protected $table = 'avaliacao.users';
+    protected $table = 'users';
 
     /**
      * The attributes that are mass assignable.
