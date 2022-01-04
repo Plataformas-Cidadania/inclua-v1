d615a584a090a7f6349b26f0a42c1609fd2b6aa9
Relison Galvão <relison@cd10.com.br>
2022-01-04 19:12:49 -0300

    INC-161

--- public/js/components/recursos/page.js
+++ public/js/components/recursos/page.js
@@ -7,27 +7,42 @@ const Page = () => {
   const [total, setTotal] = useState(0);
   const [page, setPage] = useState(0);
   const [perPage, setPerpage] = useState(12);
+  const [menuItens, setSearch] = useState({
+    id: 1,
+    title: "Categoria",
+    txt: 'Busque por categoria',
+    rota: 'recurso/categoria/',
+    type: true
+  });
+  const [menuLi, setMenuLi] = useState(1);
+  const [listMenu, setListMenu] = useState([]);
+  const [spinList, setspinList] = useState(false);
   const menu = [{
     id: 1,
-    title: "Tema",
-    txt: 'Busque por tema',
-    rota: ''
+    title: "Categoria",
+    txt: 'Busque por categoria',
+    rota: 'api/recurso/categoria/',
+    type: true
   }, {
     id: 2,
     title: "Tipo",
     txt: 'Busque por tipo',
-    rota: ''
+    rota: 'api/recurso/tipo_recurso/',
+    type: true
   }, {
     id: 3,
     title: "Palavra-chave",
     txt: 'Busque por palavra-chave',
-    rota: ''
+    rota: 'api/recurso/palavra_chave/',
+    type: false
   }, {
     id: 4,
-    title: "Classificação",
-    txt: 'Busque por Classificação',
-    rota: ''
-  }];
+    title: "Indicador",
+    txt: 'Busque por indicador',
+    rota: '',
+    type: true
+  } //{id: 5, title: "Autores", txt: 'Busque por autores', rota: 'recurso/autores/{recurso}'},
+  ];
   useEffect(() => {
     Recurso();
   }, []);
@@ -42,7 +57,6 @@ const Page = () => {
           page: page + 1
         }
       });
-      console.log('data', result.data);
       setRecursos(result.data.data);
       setTotal(result.data.total);
     } catch (error) {
@@ -50,8 +64,26 @@ const Page = () => {
     }
   };
 
-  const btnSearch = (id, txt, rota) => {
-    console.log(id, txt, rota);
+  const handleSearch = async e => {
+    setspinList(false);
+    const search = e.target.value ? e.target.value : ' ';
+
+    try {
+      setspinList(true);
+      const result = await axios.get(menuItens.rota + search, {});
+      console.log('-----', result.data.data);
+      setListMenu(result.data.data);
+      setRecursos(result.data.data);
+      setTotal(result.data.total);
+    } catch (error) {
+      setspinList(false);
+      console.log(error);
+    }
+  };
+
+  const btnSearch = item => {
+    setSearch(item);
+    setMenuLi(item.id);
   };
 
   return /*#__PURE__*/React.createElement("div", {
@@ -64,13 +96,17 @@ const Page = () => {
     className: "col-md-3"
   }), /*#__PURE__*/React.createElement("div", {
     className: "col-md-6"
+  }, /*#__PURE__*/React.createElement("div", {
+    style: {
+      position: "absolute"
+    }
   }, /*#__PURE__*/React.createElement("ul", {
-    className: "menu-small mb-2"
+    className: "menu-small  mb-2"
   }, menu.map((item, key) => {
     return /*#__PURE__*/React.createElement("li", {
-      className: "cursor",
+      className: "cursor " + (menuLi === item.id ? 'menu-small-on' : ''),
       key: 'menu_' + key,
-      onClick: () => btnSearch(item.id, item.txt, item.rota)
+      onClick: () => btnSearch(item)
     }, item.title);
   })), /*#__PURE__*/React.createElement("div", {
     className: "input-icon"
@@ -78,19 +114,35 @@ const Page = () => {
     id: "ativarBox",
     type: "text",
     className: "form-control",
-    placeholder: "Busque um ...."
+    placeholder: menuItens.txt,
+    onChange: handleSearch,
+    autoComplete: "off"
   }), /*#__PURE__*/React.createElement("i", {
     className: "fas fa-search"
-  })), /*#__PURE__*/React.createElement("ul", {
+  })), /*#__PURE__*/React.createElement("div", {
+    style: {
+      display: menuItens.type ? '' : 'none'
+    }
+  }, /*#__PURE__*/React.createElement("ul", {
     className: "box-search-itens box-busca"
   }, /*#__PURE__*/React.createElement("div", {
-    className: "text-center"
-  }, /*#__PURE__*/React.createElement("img", {
-    src: "/img/load.gif",
-    alt: "",
-    width: "60",
-    className: "login-img"
-  }))), /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("br", null)), /*#__PURE__*/React.createElement("div", {
+    className: "fa-3x text-center",
+    style: {
+      display: spinList ? '' : 'none'
+    }
+  }, /*#__PURE__*/React.createElement("i", {
+    className: "fas fa-circle-notch fa-spin"
+  })), /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("ul", {
+    className: "list-search"
+  }, listMenu.map((item, key) => {
+    return /*#__PURE__*/React.createElement("li", {
+      className: "cursor ",
+      key: 'list_' + key //onClick={() => btnSearch(item)}
+
+    }, item.nome);
+  }))))), /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("br", null)), /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("br", null)), /*#__PURE__*/React.createElement("div", {
+    className: "col-md-3"
+  }), /*#__PURE__*/React.createElement("div", {
     className: "col-md-12"
   }, /*#__PURE__*/React.createElement("p", {
     style: {
