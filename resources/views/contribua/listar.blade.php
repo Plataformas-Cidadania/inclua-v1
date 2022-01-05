b269e23622022089737d09a502b18f46f4010972
Relison Galvão <relison@cd10.com.br>
2022-01-05 01:06:57 -0300

    INC-107

--- resources/views/contribua/listar.blade.php
+++ resources/views/contribua/listar.blade.php
@@ -18,69 +18,79 @@
         <div class="p-3">&nbsp;</div>
     </div>
 
-    <div class="container">
-        <div class="row">
-            <div class="col-md-1"></div>
-            <div class="col-md-4">
-                <h2>Já tenho cadastro</h2>
-                @include('auth.login')
-            </div>
-            <div class="col-md-2"></div>
-            <div class="col-md-4">
-                <h2>Criar meu cadastro</h2>
-                @include('auth.register')
-            </div>
-            <div class="col-md-1"></div>
-        </div>
-    </div>
 
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4">
-                <a href="interaja">
-                    <div class="dorder-container">
-                        <div class="dorder-container-mai p-4 text-center">
-                            <i class="far fa-hand-point-up fa-3x"></i><br><br>
-                            <h2>Interaja</h2>
-                            <p>Dissemine informações, proponha discussões, ajude quem necessita</p>
+
+
+
+    @if((array)auth()->user())
+        <h2>Olá, {{auth()->user()->name}}!</h2>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <a href="interaja">
+                        <div class="dorder-container">
+                            <div class="dorder-container-mai p-4 text-center">
+                                <i class="far fa-hand-point-up fa-3x"></i><br><br>
+                                <h2>Interaja</h2>
+                                <p>Dissemine informações, proponha discussões, ajude quem necessita</p>
+                            </div>
                         </div>
-                    </div>
-                </a>
-            </div>
-            <div class="col-md-4">
-                <a href="compartilhe">
-                    <div class="dorder-container">
-                        <div class="dorder-container-mai p-4 text-center">
-                            <i class="fas fa-share-alt fa-3x"></i><br><br>
-                            <h2>Compartilhe</h2>
-                            <p>Ajude a ampliar os recursos, compartilhe sua experiência.</p>
-                        </div>
-                    </div>
-                </a>
-            </div>
-            <div class="col-md-4">
-                <a href="relate">
-                    <div class="dorder-container">
-                        <div class="dorder-container-mai p-4 text-center">
-                            <i class="fas fa-bullhorn fa-3x"></i><br><br>
-                            <h2>Relate</h2>
-                            <p>Como a INCLUA te ajudou? Conte-nos sua experiência</p>
-                        </div>
-                    </div>
-                </a>
-            </div>
-            {{--<div class="col-md-3">
-                <div class="dorder-container">
-                    <div class="dorder-container-mai p-4 text-center">
-                        <i class="fas fa-users fa-3x"></i><br><br>
-                        <h2>Comunidade</h2>
-                        <p>Construa uma rede colaborativa visando o aperfeiçoamento mútuo</p>
-                    </div>
+                    </a>
                 </div>
-            </div>--}}
-            <div class="col-md-12"><br><br></div>
+                <div class="col-md-4">
+                    <a href="compartilhe">
+                        <div class="dorder-container">
+                            <div class="dorder-container-mai p-4 text-center">
+                                <i class="fas fa-share-alt fa-3x"></i><br><br>
+                                <h2>Compartilhe</h2>
+                                <p>Ajude a ampliar os recursos, compartilhe sua experiência.</p>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-4">
+                    <a href="relate">
+                        <div class="dorder-container">
+                            <div class="dorder-container-mai p-4 text-center">
+                                <i class="fas fa-bullhorn fa-3x"></i><br><br>
+                                <h2>Relate</h2>
+                                <p>Como a INCLUA te ajudou? Conte-nos sua experiência</p>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                {{--<div class="col-md-3">
+                    <div class="dorder-container">
+                        <div class="dorder-container-mai p-4 text-center">
+                            <i class="fas fa-users fa-3x"></i><br><br>
+                            <h2>Comunidade</h2>
+                            <p>Construa uma rede colaborativa visando o aperfeiçoamento mútuo</p>
+                        </div>
+                    </div>
+                </div>--}}
+                <div class="col-md-12"><br><br></div>
+            </div>
         </div>
-    </div>
+    @else
+        <div class="container">
+            <div class="row">
+                <div class="col-md-1"></div>
+                <div class="col-md-4">
+                    <h2>Já tenho cadastro</h2>
+                    @include('auth.login')
+                </div>
+                <div class="col-md-2"></div>
+                <div class="col-md-4">
+                    <h2>Criar meu cadastro</h2>
+                    @include('auth.register')
+                </div>
+                <div class="col-md-1"></div>
+            </div>
+        </div>
+
+    @endif
+
+
 
 
 
