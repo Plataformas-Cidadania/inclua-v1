328f737deaba4ad0d23a61e23109750a1ef68900
Bruno Passos <brpassos.programador@gmail.com>
2022-01-11 18:02:31 -0300

    INC-155

--- packages/cms/resources/views/formato-recurso/_form.blade.php
+++ packages/cms/resources/views/formato-recurso/_form.blade.php
@@ -1,5 +1,5 @@
 
 
-<label for="titulo">Nome</label>
+<label for="nome">Nome</label>
 <input type="text" name="nome" class="form-control width-medio <% validar(formatoRecurso.nome)%>" ng-model="formatoRecurso.nome" ng-required="true">
 
