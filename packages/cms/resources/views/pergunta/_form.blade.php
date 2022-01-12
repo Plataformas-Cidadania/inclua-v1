832134b35b0bdbcafd8785774cb1a581d4f5ea19
Bruno Passos <brpassos.programador@gmail.com>
2022-01-12 16:40:42 -0300

    INC-119

--- packages/cms/resources/views/pergunta/_form.blade.php
+++ packages/cms/resources/views/pergunta/_form.blade.php
@@ -1,37 +1,37 @@
 <input type="hidden" name="letra" ng-model="pergunta.letra">
-<input type="hidden" name="id_pergunta" ng-model="pergunta.id_indicador"><br>
+<input type="hidden" name="id_indicador" ng-model="pergunta.id_indicador"><br>
 <br><% pergunta.letra %><br>
 <label for="descricao">Descricao</label>
-<input type="text" name="descricao" class="form-control width-medio <% validar(pergunta.descricao)%>" ng-model="pergunta.descricao" ng-required="true">
-
+<input type="text" name="descricao" class="form-control width-grande <% validar(pergunta.descricao)%>" ng-model="pergunta.descricao" ng-required="true">
+<br>
 <label for="legenda">Legenda</label>
-<input type="text" name="legenda" class="form-control width-medio <% validar(pergunta.legenda)%>" ng-model="pergunta.legenda" ng-required="true">
-
+<input type="text" name="legenda" class="form-control width-grande <% validar(pergunta.legenda)%>" ng-model="pergunta.legenda" ng-required="true">
+<br>
 <label for="tipo">Tipo</label>
-<select name="tipo" id="tipo" ng-model="pergunta.tipo">
+<select name="tipo" id="tipo" class="form-control width-pequeno <% validar(pergunta.tipo)%>" ng-model="pergunta.tipo">
     <option value="1">Sim/Não</option>
     <option value="2">Nota</option>
     <option value="3">Range</option>
 </select>
-
+<br>
 <label for="nao_se_aplica">Opção de Não se Aplica</label>
-<select name="nao_se_aplica" id="nao_se_aplica" ng-model="pergunta.nao_se_aplica">
+<select name="nao_se_aplica" id="nao_se_aplica" class="form-control width-pequeno <% validar(pergunta.nao_se_aplica)%>" ng-model="pergunta.nao_se_aplica">
     <option value="0">Não possui essa opção</option>
     <option value="1">Possui essa opção</option>
 </select>
-
+<br>
 <label for="inverter">Inverter Valores</label>
-<select name="inverter" id="inverter" ng-model="pergunta.inverter">
+<select name="inverter" id="inverter" class="form-control width-pequeno <% validar(pergunta.inverter)%>" ng-model="pergunta.inverter">
     <option value="0">Não</option>
     <option value="1">Sim</option>
 </select>
-
+<br>
 <label for="vl_subPergunta">Valor de ativação de subpergunta</label>
-<input type="text" name="vl_subPergunta" id="vl_subPergunta" ng-model="pergunta.vlSubPergunta">
-
+<input type="text" name="vl_subPergunta" id="vl_subPergunta" class="form-control width-medio <% validar(pergunta.vlSubpergunta)%>" ng-model="pergunta.vlSubPergunta">
+<br>
 <label for="vl_minimo">Valor Mínimo</label>
 <input type="number" name="vl_minimo" class="form-control width-pequeno <% validar(pergunta.vl_minimo)%>" ng-model="pergunta.vl_minimo" ng-required="true">
-
+<br>
 <label for="vl_maximo">Valor Máximo</label>
 <input type="number" name="vl_maximo" class="form-control width-pequeno <% validar(pergunta.vl_maximo)%>" ng-model="pergunta.vl_maximo" ng-required="true">
-
+<br>
