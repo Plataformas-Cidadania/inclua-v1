d5bb6f97f3a2d0536acdf491418031a9cccff54f
Bruno Passos <brpassos.programador@gmail.com>
2022-01-17 15:28:08 -0300

    INC-137

---
+++ packages/cms/resources/assets/js/controllers/alterarRecursoCtrl.js
@@ -0,0 +1,112 @@
+cmsApp.controller('alterarCategoriaCtrl', ['$scope', '$http', 'Upload', '$timeout', function($scope, $http, Upload, $timeout){
+
+    $scope.processandoSalvar = false;
+    $scope.processandoDetalhar = false;
+
+
+    //$scope.id_categoria = 0;
+    $scope.dimensoes = [];
+    $scope.dimensao = null;
+
+    //ALTERAR/////////////////////////////
+
+    $scope.tinymceOptions = tinymceOptions;
+
+    $scope.mostrarForm = false;
+
+    $scope.removerImagem = false;
+
+
+    $scope.detalhar = function(id){
+        $scope.processandoDetalhar = true;
+        $http({
+            url: 'api/categoria/'+id,
+            method: 'GET',
+            params: {
+
+            }
+        }).success(function(data, status, headers, config){
+            $scope.categoria = data.data;
+            console.log($scope.categoria);
+            $scope.dimensoes.forEach(function (item){
+                if(item.id_dimensao === $scope.categoria.id_dimensao){
+                    $scope.dimensao = item;
+                }
+            });
+            $scope.processandoDetalhar = false;
+        }).error(function(data){
+            $scope.message = "Ocorreu um erro: "+data;
+            $scope.processandoDetalhar = false;
+        });
+    };
+
+    $scope.alterar = function (file){
+
+        if(file==null){
+
+            $scope.processandoSalvar = true;
+            $http.put("api/categoria/"+$scope.id, $scope.categoria).success(function (data){
+                //console.log(data);
+                $scope.processandoSalvar = false;
+                $scope.mensagemSalvar = data.message;
+                $scope.removerImagem = false;
+            }).error(function(data){
+                //console.log(data);
+                $scope.mensagemSalvar = "Ocorreu um erro: "+data;
+                $scope.processandoSalvar = false;
+            });
+
+        }else{
+
+            file.upload = Upload.upload({
+                url: 'api/categoria/'+$scope.id,
+                data: {text: $scope.text, file: file},
+            });
+
+            file.upload.then(function (response) {
+                $timeout(function () {
+                    file.result = response.data;
+                });
+                $scope.picFile = null;//limpa o form
+                $scope.mensagemSalvar =  "Gravado com sucesso!";
+                $scope.removerImagem = false;
+                $scope.imagemBD = 'imagens/texts/'+response.data;
+                console.log($scope.imagemDB);
+            }, function (response) {
+                if (response.status > 0){
+                    $scope.errorMsg = response.status + ': ' + response.data;
+                }
+            }, function (evt) {
+                //console.log(evt);
+                // Math.min is to fix IE which reports 200% sometimes
+                file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
+            });
+
+        }
+
+    };
+
+    $scope.limparImagem = function(){
+        $scope.picFile = null;
+        $scope.imagemBD = null;
+        $scope.removerImagem = true;
+    };
+
+    $scope.carregaImagem  = function(img) {
+        if(img!=''){
+            $scope.imagemBD = 'imagens/categorias/xs-'+img;
+            //console.log($scope.imagemBD);
+        }
+    };
+
+    $scope.validar = function(valor) {
+        if(valor===undefined && $scope.form.$dirty){
+            return "campo-obrigatorio";
+        }
+        return "";
+    };
+    /////////////////////////////////
+
+
+
+}]);
