4d28ba0a27e5fab3fb9f298ddc7e94efa3e8d554
Bruno Passos <brpassos.programador@gmail.com>
2022-01-26 10:18:55 -0300

    INC-143

---
+++ packages/cms/resources/assets/js/controllers/linkCtrl.js
@@ -0,0 +1,217 @@
+cmsApp.controller('linkCtrl', ['$scope', '$http', 'Upload', '$timeout', function($scope, $http, Upload, $timeout){
+
+
+    $scope.link = {
+        numero: 2,
+        titulo: 'aaaa',
+        descricao: 'aaaaaaaaaaa',
+        vl_baixo: 1,
+        vl_alto: 2,
+    };
+    $scope.links = [];
+    $scope.dimensoes = [];
+    $scope.dimensao = null;
+    $scope.currentPage = 1;
+    $scope.lastPage = 0;
+    $scope.totalItens = 0;
+    $scope.maxSize = 5;
+    $scope.itensPerPage = 100;
+    $scope.dadoPesquisa = '';
+    $scope.campos = "id_link, nome";
+    $scope.campoPesquisa = "titulo";
+    $scope.processandoListagem = false;
+    $scope.processandoExcluir = false;
+    $scope.ordem = "numero";
+    $scope.sentidoOrdem = "asc";
+    var $listar = false;//para impedir de carregar o conteúdo dos watchs no carregamento da página.
+
+    $scope.$watch('currentPage', function(){
+        if($listar){
+            listarLinks();
+        }
+    });
+    $scope.$watch('itensPerPage', function(){
+        if($listar){
+            listarLinks();
+        }
+    });
+    $scope.$watch('dadoPesquisa', function(){
+        if($listar){
+            listarLinks();
+        }
+    });
+
+
+
+    var listarDimensoes = function(){
+        $scope.processandoListagem = true;
+        $http({
+            url: 'api/dimensao',
+            method: 'GET',
+            params: {
+
+            }
+        }).success(function(data, status, headers, config){
+            console.log(data.data);
+            $scope.dimensoes = data.data;
+        }).error(function(data){
+            $scope.message = "Ocorreu um erro: "+data;
+        });
+    }
+
+    var listarLinks = function(){
+        $scope.processandoListagem = true;
+        $http({
+            url: 'api/link',
+            method: 'GET',
+            params: {
+                page: $scope.currentPage,
+                itensPorPagina: $scope.itensPerPage,
+                dadoPesquisa: $scope.dadoPesquisa,
+                campos: $scope.campos,
+                campoPesquisa: $scope.campoPesquisa,
+                ordem: $scope.ordem,
+                sentido: $scope.sentidoOrdem
+            }
+        }).success(function(data, status, headers, config){
+            console.log(data.data);
+            $scope.links = data.data;
+            let numeroMaximo = Math.max.apply(Math, $scope.links.map(function(item) { return item.numero; }));
+            $scope.link.numero = numeroMaximo + 1;
+            $scope.lastPage = data.last_page;
+            $scope.totalItens = data.data.length;
+            //$scope.totalItens = data.total;
+            $scope.primeiroDaPagina = data.from;
+            $scope.ultimoDaPagina = data.to;
+            $listar = true;
+            //console.log(data);
+            $scope.processandoListagem = false;
+        }).error(function(data){
+            $scope.message = "Ocorreu um erro: "+data;
+            $scope.processandoListagem = false;
+        });
+    };
+
+    $scope.ordernarPor = function(ordem){
+        $scope.ordem = ordem;
+        //console.log($scope.ordem);
+        if($scope.sentidoOrdem=="asc"){
+            $scope.sentidoOrdem = "desc";
+        }else{
+            $scope.sentidoOrdem = "asc";
+        }
+
+        listarLinks();
+    };
+
+    $scope.validar = function(){
+
+    };
+
+
+    listarLinks();
+    listarDimensoes();
+
+    //INSERIR/////////////////////////////
+
+    $scope.tinymceOptions = tinymceOptions;
+    $scope.mostrarForm = false;
+    $scope.processandoInserir = false;
+
+    $scope.inserir = function (file, arquivo){
+
+        $scope.mensagemInserir = "";
+
+        if(file==null && arquivo==null){
+            $scope.processandoInserir = true;
+            $http.post("api/link", $scope.link).success(function (data){
+                 listarLinks();
+                 //delete $scope.link;//limpa o form
+                $scope.link = {};//limpa o form
+                $scope.mensagemInserir =  "Gravado com sucesso!";
+                $scope.processandoInserir = false;
+             }).error(function(data){
+                $scope.mensagemInserir = "Ocorreu um erro!";
+                $scope.processandoInserir = false;
+             });
+        }else{
+
+            link.file = file;
+            link.arquivo = arquivo;
+            Upload.upload({
+                url: 'api/link',
+                data: link,
+                //data: {link: $scope.link, file: file, arquivo: arquivo},
+            }).then(function (response) {
+                $timeout(function () {
+                    $scope.result = response.data;
+                });
+                console.log(response.data);
+                delete $scope.link;//limpa o form
+                $scope.picFile = null;//limpa o file
+                $scope.fileArquivo = null;//limpa o file
+                listarLinks();
+                $scope.mensagemInserir =  "Gravado com sucesso!";
+            }, function (response) {
+                console.log(response.data);
+                if (response.status > 0){
+                    $scope.errorMsg = response.status + ': ' + response.data;
+                }
+            }, function (evt) {
+                //console.log(evt);
+                // Math.min is to fix IE which reports 200% sometimes
+                $scope.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
+            });
+        }
+
+    };
+
+    $scope.limparImagem = function(){
+        delete $scope.picFile;
+        $scope.form.file.$error.maxSize = false;
+    };
+
+    $scope.validar = function(valor) {
+        //console.log(valor);
+        if(valor===undefined){
+            return "campo-obrigatorio";
+        }
+        return "";
+    };
+    /////////////////////////////////
+
+    //EXCLUIR/////////////////////////
+    $scope.perguntaExcluir = function (id, titulo, imagem){
+        $scope.idExcluir = id;
+        $scope.tituloExcluir = titulo;
+        $scope.imagemExcluir = imagem;
+        $scope.excluido = false;
+        $scope.mensagemExcluido = "";
+    }
+
+    $scope.excluir = function(id){
+        $scope.processandoExcluir = true;
+        $http({
+            url: 'api/link/'+id,
+            method: 'DELETE'
+        }).success(function(data, status, headers, config){
+            console.log(data);
+            if(data.success){
+                $scope.processandoExcluir = false;
+                $scope.excluido = true;
+                $scope.mensagemExcluido = data.message;
+                listarLinks();
+                return;
+            }
+            $scope.processandoExcluir = false;
+            $scope.excluido = false;
+            $scope.mensagemExcluido = data.message;
+        }).error(function(data){
+            $scope.message = "Ocorreu um erro: "+data;
+            $scope.processandoExcluir = false;
+            $scope.mensagemExcluido = "Erro ao tentar excluir!";
+        });
+    };
+
+
+}]);
