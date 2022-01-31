5229f7983ec22c8a8543c3971af6143f0fb7da79
Bruno Passos <brpassos.programador@gmail.com>
2022-01-31 11:09:09 -0300

    INC-175

---
+++ packages/cms/resources/views/depoimento/_form.blade.php
@@ -0,0 +1,5 @@
+
+
+<label for="titulo">Depoimento</label>
+<input type="text" name="nome" class="form-control width-grande <% validar(depoimento.nome)%>" ng-model="depoimento.descricao" ng-required="true">
+
