864e461e892a4022a8cb1add9ae5a9c4c9a460c7
Bruno Passos <brpassos.programador@gmail.com>
2022-02-03 16:12:17 -0300

    INC-212

---
+++ packages/cms/resources/views/resposta-relate/_form.blade.php
@@ -0,0 +1,5 @@
+
+
+<label for="descricao">Descrição</label>
+<input type="text" name="descricao" class="form-control width-medio <% validar(resposta.descricao)%>" ng-model="resposta.descricao" ng-required="true">
+
