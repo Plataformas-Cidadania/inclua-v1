45b4642ed21e84f883f2671a7a16abe54c4e7c39
Bruno Passos <brpassos.programador@gmail.com>
2022-02-11 20:52:30 -0300

    INC-226

--- public/js/components/depoimento/index.js
+++ public/js/components/depoimento/index.js
@@ -1,5 +1,5 @@
 const Depoimento = () => {
-  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement(Insert, null));
+  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement(List, null));
 };
 
 ReactDOM.render( /*#__PURE__*/React.createElement(Depoimento, null), document.getElementById('depoimento'));
