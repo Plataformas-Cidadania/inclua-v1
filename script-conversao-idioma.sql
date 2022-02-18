fc55111ddb7b75062277ae92e8464200c864523f
Thiago Giannini Ramos <gianninimail@gmail.com>
2022-02-18 09:16:37 -0300

    Adição do script de conversão do Idioma para siglas

---
+++ script-conversao-idioma.sql
@@ -0,0 +1,39 @@
+update avaliacao.link
+set  idioma = 'PT-BR|EN'
+where idioma = 'Portuguęs/Inglęs';
+
+update avaliacao.link
+set  idioma = 'PT-BR'
+where idioma = 'Portuguęs';
+
+update avaliacao.link
+set  idioma = 'EN'
+where idioma = 'Inglęs';
+
+update avaliacao.link
+set  idioma = 'EN|PT-BR'
+where idioma = 'Inglęs/Portuguęs';
+
+update avaliacao.link
+set  idioma = 'EN|IT|TR|EL'
+where idioma = 'Inglęs/Italiano/Turco/Grego';
+
+update avaliacao.link
+set  idioma = 'EN|DE'
+where idioma = 'Inglęs/Alemăo';
+
+update avaliacao.link
+set  idioma = 'PT-BR|Línguas Indígenas'
+where idioma = 'Portuguęs/Línguas Indígenas';
+
+update avaliacao.link
+set  idioma = 'PT-BR|ES|EN'
+where idioma = 'Portuguęs/Espanhol/Inglęs';
+
+update avaliacao.link
+set  idioma = 'PT-BR'
+where idioma = 'Portugęs';
+
+update avaliacao.link
+set  idioma = 'ES'
+where idioma = 'Espanhol';
